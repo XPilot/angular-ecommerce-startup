146433d2f79ff263adb89a818318d23b1e9efd30
Radu Paraschiv <xpilot@gmail.com>
2016-04-08 00:54:36 +0300

    Add application entrypoint

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,1 +1,35 @@
-export default () => {};
+import { Component } from 'angular2/core';
+import { RouteConfig, Router, RouterOutlet } from 'angular2/router';
+
+// routes import
+import Routes from './app.routes';
+
+import {Homepage} from './pages/';
+import ProductItem from './components/product-item/product-item';
+
+// scss styles
+import './assets/scss/main.scss';
+
+@Component({
+  selector: 'app',
+  providers: [],
+  directives: [RouterOutlet],
+  template:
+  `
+    <div class="Layout">
+      <router-outlet></router-outlet>
+    </div>
+  `
+})
+
+@RouteConfig([{
+    path: '/',
+    name: 'Homepage',
+    component: Homepage,
+    useAsDefault: true,
+  }
+])
+
+export default class App {
+  constructor() {}
+}

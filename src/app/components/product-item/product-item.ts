0b1c9a6adf4a663b3039fc511f0e4a64bfbf0175
Radu Paraschiv <xpilot@gmail.com>
2016-04-08 00:57:33 +0300

    Add product item component

---
+++ src/app/components/product-item/product-item.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from 'angular2/core';
+
+// scss styles
+import './product-item.scss';
+
+@Component({
+  selector: 'product-item',
+  template: require('./product-item.html'),
+  providers: [],
+  directives: [],
+})
+
+export default class ProductItem {
+  @Input() productId:number;
+  @Input() productIsSelected:boolean;
+  @Input() productIsTopSeller:boolean;
+  @Input() productHeading:string;
+  @Input() productList:Array<Object>;
+  @Input() productMainFeatures:Array<string>;
+  @Input() productSecondaryFeatures:Array<string>;
+  @Input() productPrice:Object;
+
+  constructor() {
+  }
+
+  getPriceValue(value) {
+    const valueSplit = value.toString().split('.');
+    const stringValue = valueSplit.length > 1 ?
+    `${valueSplit[0]},<sup>${valueSplit[1]}</sup>&euro;`
+    :
+    `${valueSplit[0]}&euro;`;
+
+    return stringValue;
+  }
+}

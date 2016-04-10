1ac8a30256d3cd9eb3532c26128760e66a0bcd78
Radu Paraschiv <xpilot@gmail.com>
2016-04-10 21:01:52 +0300

    Cleanup app bootstrap

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,23 +1,18 @@
 import { bootstrap } from 'angular2/platform/browser';
 import { ROUTER_PROVIDERS } from 'angular2/router';
-import { provideHmrState, hotModuleReplacement } from 'angular2-hmr';
 
 import App from './app/app';
 
-export function main(initialState= {}) {
-  let APP_PROVIDERS = [
-    provideHmrState(initialState)
-  ]; // act as middleware
-
+export function main(initialState?: any): Promise<any> {
   return bootstrap(App, [
     ROUTER_PROVIDERS,
-    APP_PROVIDERS
   ])
   .catch(err => console.error(err));
 }
 
 if (ENV === 'development' && HMR === true) {
-  hotModuleReplacement(main, module);
+  let hmr = require('angular2-hmr');
+  hmr.hotModuleReplacement(main, module);
 } else {
   document.addEventListener('DOMContentLoaded', () => main());
 }

33b32ad38546ab82c75e99a029a479402b93157a
Radu Paraschiv <xpilot@gmail.com>
2016-04-11 10:52:29 +0300

    Add new product functions to cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -2,12 +2,38 @@ import { Injectable } from 'angular2/core';
 
 @Injectable()
 export default class CartService {
-  private products:Array<Object> = [];
+  private products:Array<any> = [];
 
-  addProduct(productId:number, productQuantity):void {
+  addProduct(product: any): Array<any> {
+    if (product) {
+      let itemExists = this.products
+        .find(item => item.id === product.id);
+
+      if (!itemExists) {
+        this.products.push({
+          id: product.id,
+          item: product,
+          quantity: 1,
+        });
+      } else {
+        this.products = this.products
+          .slice(0)
+          .map(item => {
+            if(item.id === product.id) {
+              item.quantity = item.quantity + 1;
+            }
+            return item;
+          });
+      }
+      return this.products;
+    }
+    return [];
   }
 
   deleteProduct() {
+  }
 
+  get cartProducts():Array<any> {
+    return this.products;
   }
 }

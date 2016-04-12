c7065c2ae5147fe726e27ef1c3176775769c7c5b
Radu Paraschiv <xpilot@gmail.com>
2016-04-12 03:53:27 +0300

    Add cart list functionality

--- src/app/components/cart-list/cart-list.ts
+++ src/app/components/cart-list/cart-list.ts
@@ -1,4 +1,8 @@
-import { Component, Input } from 'angular2/core';
+// deps
+import { Component, Input, Output, EventEmitter } from 'angular2/core';
+
+// components
+import InputQuantity from 'app/components/input-quantity/input-quantity';
 
 // scss styles
 import './cart-list.scss';
@@ -7,12 +11,26 @@ import './cart-list.scss';
   selector: 'cart-list',
   template: require('./cart-list.html'),
   providers: [],
-  directives: [],
+  directives: [InputQuantity],
 })
 
 export default class CartList {
   @Input() cartProducts: Array<Object>;
 
+  @Output() onProductUpdate = new EventEmitter();
+  @Output() onProductDelete = new EventEmitter();
+
   constructor() {
+  }
+
+  onQuantityChange(id, quantity) {
+    this.onProductUpdate.emit({
+      id,
+      quantity,
+    });
+  }
+
+  onDelete(id) {
+    this.onProductDelete.emit({id});
   }
 }
